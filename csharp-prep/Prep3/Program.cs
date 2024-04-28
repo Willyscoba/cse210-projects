178c68dc617cee6a1afa85ec7df9a4ccc8b6cc28
Willyscoba <williamssimeon2@email.com>
2024-04-28 23:40:16 +0100

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,68 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //For the first and second part where the user have to provide the magic number...
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        //while (true)
+        //{
+            //Console.Write("What is your guess? ");
+            //int guess = int.Parse(Console.ReadLine());
+
+            //if (guess < magicNumber)
+            //{
+                //Console.WriteLine("Higher");
+            //}
+            //else if (guess >magicNumber)
+            //{
+                //Console.WriteLine("Lower");
+            //}
+            //else
+            //{
+                //Console.WriteLine("You guessed it!");
+                //break;
+            //}
+        //}
+
+        //for the 3rd part.
+        
+        Random randomGenerator = new Random();
+        string playAgain;
+        {
+            int magicNumber = randomGenerator.Next(1, 101);
+            int Guesses = 0;
+
+            while (true)
+            {
+                Console.Write("What is your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+                Guesses++;
+
+                if (magicNumber > guess)
+                {
+                Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+            }
+            Console.WriteLine("Number of guesses: " + Guesses);
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+        } while (playAgain == "yes");
+    
+        Console.WriteLine("Thanks for playing!");
+        
     }
 }
